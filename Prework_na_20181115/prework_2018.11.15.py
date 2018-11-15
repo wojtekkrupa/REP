2a018aca30113fa2ba07fc63daa462116b5bc876
wojtekkrupa <43648014+wojtekkrupa@users.noreply.github.com>
2018-11-15 16:29:35 +0100

    Add files via upload

--- Prework_na_20181115/prework_2018.11.15.py
+++ Prework_na_20181115/prework_2018.11.15.py
@@ -19,52 +19,34 @@ with open(sciezka_nazwa_pliku_zrodlo, 'r') as plik_zrodlowy:
 
     ile_znakow = len(tekst_maly)
     komunikat2 = f"Dlugość tekstu to {ile_znakow} znaków"
-    # print(komunikat2)
+
     # ----------------------------------------------------------------------------------------
-    # from collections import defaultdict
-    # wyrazy_wg_długosci = defaultdict(list)
-    # for slowo in (tekst_podzielony):
-    #     wyrazy_wg_długosci[key].append(slowo)
-    # print(wyrazy_wg_długosci)
+    slowa_o_2_lub_wiecej_samogloskach = {}
+    for slowo in tekst_podzielony:
+        count = 0
+        for litera in slowo:
+            if litera in 'aeiouy':
+                count += 1
+        if count >= 2:
+            slowa_o_2_lub_wiecej_samogloskach[slowo] = count
+
+    wyrazy_z_samogloskami = (len(slowa_o_2_lub_wiecej_samogloskach))
+    komunikat3 = f"{wyrazy_z_samogloskami} wyrazów zawiera co najmniej dwie samogłoski"
+
     # ----------------------------------------------------------------------------------------
-    wyrazy_wg_długosci = {}
+    wyrazy_wg_dlugosci = {}
     for slowo in (tekst_podzielony):
         klucz = len(slowo)
-        if klucz not in wyrazy_wg_długosci:
-            wyrazy_wg_długosci[klucz] = []
-        wyrazy_wg_długosci[klucz].append(slowo)
-    print(wyrazy_wg_długosci)
-    srednia_dl_wyrazu = sum(wyrazy_wg_długosci.keys()) // len(wyrazy_wg_długosci)
+        if klucz not in wyrazy_wg_dlugosci:
+            wyrazy_wg_dlugosci[klucz] = []
+        wyrazy_wg_dlugosci[klucz].append(slowo)
+
+    srednia_dl_wyrazu = sum(wyrazy_wg_dlugosci.keys()) // len(wyrazy_wg_dlugosci)
     komunikat4 = f"średnia długość wyrazu to {srednia_dl_wyrazu} znaków"
-    #print(komunikat4)
-    # ----------------------------------------------------------------------------------------
-    # slownik_dlugosci_wyrazow = {}
-    # for slowo in tekst_podzielony:
-    #     klucz = len(slowo)
-    #     if klucz not in slownik_dlugosci_wyrazow:
-    #         slownik_dlugosci_wyrazow[klucz] = []
-    #     slownik_dlugosci_wyrazow[klucz].append(slowo)
-    #
-    # srednia_dl_wyrazu = sum(slownik_dlugosci_wyrazow.keys()) // len(slownik_dlugosci_wyrazow)
-    # komunikat4 = f"średnia długość wyrazu to {srednia_dl_wyrazu} znaków"
-    # print(komunikat4)
+
     # ----------------------------------------------------------------------------------------
     append_to_file(sciezka_nazwa_pliku_wynik,
-                   komunikat1+'\n'+
-                   komunikat2+'\n'+
+                   komunikat1 +'\n' +
+                   komunikat2 +'\n' +
+                   komunikat3 +'\n' +
                    komunikat4)
-    # ----------------------------------------------------------------------------------------
-    # slownik_slow_z_samogloskami = {}
-    # for slowo in tekst_podzielony:
-    #     licznik = 0
-    #
-    #     for litera in slowo:
-    #
-    #         if litera in 'aeiouy':
-    #             licznik += 1
-    #     print(slowo, end=" ")
-    #     print(licznik)
-    #
-    #     if licznik not in slownik_slow_z_samogloskami:
-    #         slownik_slow_z_samogloskami.append(slowo)
-
